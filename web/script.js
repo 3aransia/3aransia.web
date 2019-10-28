7934c7f79e04a58e02d996cbaf91c0c87bef8114
Amine M. Boulouma <amine@boulouma.com>
2019-10-28 23:55:27 +0100

    Updated version 0.1 of the translator

---
+++ web/script.js
@@ -0,0 +1,19 @@
+var BASE_API_ROUTE = "https://api3aransia.herokuapp.com/"
+var TRANSLATION_API_ROUTE = BASE_API_ROUTE + "translate_moroccan_arabic/"
+
+$(document).ready(function(){
+    $('#translate').click(function() {
+        $("#translation").text("")
+        var sourceText = $("#source-text").val()
+        var parsedSourceText = sourceText.replace(new RegExp(" ", 'g'), '+');
+        $.getJSON(TRANSLATION_API_ROUTE + parsedSourceText, function (result) {
+            $.each(result, function (i, field) {
+                var textNode = document.createTextNode(i+ " " +JSON.stringify(field));
+                console.log(textNode) 
+                console.log(result) 
+                $("#translation").append(field.arabian_word + " ");
+            });
+        });
+        // $("#translation").text(response)
+    });
+});
